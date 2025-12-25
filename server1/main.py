a9e77921c0e3f11c59a94ecad6f4d744803d8b80
Yehuda_Gabrieli <yehuda8009@gmail.com>
2025-12-25 10:21:11 +0200

    finish server 1

---
+++ server1/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+DB_PATH = "/app/db/shopping_list.json"
+
+
+@app.get("/items")
+def get_items():
+    try:
+        with open(DB_PATH, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+
+@app.post("/items")
+def add_item(name: str, amount: int):
+    with open(DB_PATH, "r") as f:
+        items = json.load(f) 
+        
+    new_id = len(items) + 1
+    new_item = {"id": new_id, "name": name, "amount": amount}
+    items.append(new_item)
+    with open(DB_PATH, "w") as f:
+        json.dump(items, f)   
+    return new_item
